74c22d4b3d52daf048eac1e436b42efb18a2499e
Davi Mota <92314721+davixaviermota@users.noreply.github.com>
2023-06-30 11:36:28 -0300

    criando o exercício novo
    E estudando Condições no JS

---
+++ ex004/condicao.js
@@ -0,0 +1,8 @@
+/*console.log('o console funcionou!!') /*aqui é para mostrar no console(por que o document.write nao funciona no console)*/ 
+
+var vel = 90
+console.log(`A velocidade do seu carro é ${vel} Km/h`) /* para mostrar lembre sempre é entre crases `` */
+
+if( vel > 60){ /*Condição simples*/ 
+    console.log(`Voce ultrapassou o limite de velocidade, Multado!`)
+}
