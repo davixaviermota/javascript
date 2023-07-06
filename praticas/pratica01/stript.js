6a3c37c21f21cb880c0897117243a63a532ecae8
Davi Mota <92314721+davixaviermota@users.noreply.github.com>
2023-07-06 20:49:53 -0300

    Fiz o script que mostrar a hora e sua foto correspondente
    Pegando a hora do sistema e usando o if e else aninhado

--- praticas/pratica01/stript.js
+++ praticas/pratica01/stript.js
@@ -1,1 +1,19 @@
-alert("Olá Mundo ")
+function carregar(){
+
+var msg = document.getElementById('msg')
+var imagem = document.getElementById('imagem') 
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `Agora são ${hora} horas`
+
+    if(hora >= 0 && hora < 12){
+    /*bom dia */
+        imagem.src = 'manha.png'
+    }else if (hora >= 12 && hora <18){
+    /*boa tarde */
+        imagem.src = 'tarde.png'
+    }else {
+    /*boa noite */
+         imagem.src = 'noite.png'
+    }
+}
