0c0ec99c8751855bce987a7f7a348af3c677f9af
Davi Mota <92314721+davixaviermota@users.noreply.github.com>
2023-07-12 11:05:06 -0300

    Update stript.js

--- praticas/pratica02/stript.js
+++ praticas/pratica02/stript.js
@@ -1,10 +1,18 @@
 function verificar(){
    var data = new Date() 
-   var ano = data.getFullYear()
-   var nasc = document.getElementById('nasc')
+   var ano = data.getFullYear() // peguei o ano completo do sistema/computador
+   var nasc = document.getElementById('nasc') //peguei o input do ano 
    var res = document.getElementById('res')
-   if (nasc.value.length == 0 || nasc.value > ano) {
+
+   if (nasc.value.length == 0 || nasc.value > ano) { //aqui é tipo uma validação, se o valor do nasc for igual a 0 ou seja se nao for digitado nada, ou ou valor do nasc ser maior que ano(2023)
+
+//vai aparecer o alerta
      window.alert('erro, verifique os dados')
+
+   } else{ //se nao
+    var sex = document.getElementsByName('sex') //peguei o inpute da bolinha de masc e femi
+    var idade = ano - Number(nasc.value) // criei var idade que vai ser = ano - o valor do nascimento 
+    res.innerHTML = `Idade calculada: ${idade}`
    }
 
 }
