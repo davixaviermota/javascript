fc1e1ec327177a56a5aaee96aa8270f0ed5047d8
Davi Mota <92314721+davixaviermota@users.noreply.github.com>
2023-07-12 11:49:26 -0300

    coloquei o script para mudar as imagens dos homens

--- praticas/pratica02/stript.js
+++ praticas/pratica02/stript.js
@@ -12,7 +12,41 @@ function verificar(){
    } else{ //se nao
     var sex = document.getElementsByName('sex') //peguei o inpute da bolinha de masc e femi
     var idade = ano - Number(nasc.value) // criei var idade que vai ser = ano - o valor do nascimento 
-    res.innerHTML = `Idade calculada: ${idade}`
+    var genero = '' //criei uma var vazia
+    var img = document.createElement('img') //criando um elemento no js, uma tag de imagem
+    img.setAttribute('id', 'foto') //aqui eu atribui a tag um id = foto
+
+    // condição para os inputs radio masculino e feminino
+    if (sex[0].checked) { //se o sex(var dos inpts) tiver marcado, ou em zero 
+      genero = 'homem' //vai ser homem/masculino
+      if(idade >= 0 && idade < 10){
+        //crianca
+        img.setAttribute('src', 'bebe-m.png')
+      }else if( idade <21){
+        //jovem
+        img.setAttribute('src', 'menino.png')
+      } else if(idade <50){
+        //adulto
+        img.setAttribute('src', 'homem.png')
+      }else{
+        //idoso
+        img.setAttribute('src', 'senhor.png')
+      }
+    }else if(sex[1].checked){ //mesma logica do de cima
+      genero = 'mulher' // só que agora vai ser mulher quando tiver marcado 1 (por que no inpute radio é enviado se agente marcou masculino ou feminino como 0 ou 1)
+      if(idade >= 0 && idade < 10){
+        //crianca
+      }else if( idade <21){
+        //jovem
+
+      } else if(idade <50){
+        //adulto
+      }else{
+        //idoso
+      }
+    }
    }
+   res.innerHTML = `Detectamos ${genero} com ${idade} de idade`
+   res.appendChild(img) //aqui é para adicionar o elemento, para as imagens aparacer
 
 }
